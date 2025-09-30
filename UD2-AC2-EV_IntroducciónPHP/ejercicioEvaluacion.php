aabcadcf7abc135fb090738659b66739a7ccf687
MarcosRSPG <marcosrs.softwaredeveloper@gmail.com>
2025-10-01 00:29:43 +0200

    Ejercicio evaluable completado

--- UD2-AC2-EV_IntroducciónPHP/ejercicioEvaluacion.php
+++ UD2-AC2-EV_IntroducciónPHP/ejercicioEvaluacion.php
@@ -11,38 +11,67 @@ function comprobarPrimo($num) {
 	return true;
 }
 
-$numero = 14;
+$numero = 27;
 
 if (comprobarPrimo((int)$numero)) {
 	echo "El número $numero es primo";
 }else{
 	echo "El número $numero no es primo";
-	$numero--;
-	if (comprobarPrimo((int)$numero)) {
-		echo "<br>El número $numero es primo";
-	}else{
-		echo "<br>El número $numero tampoco es primo";
-	}
+}
+$numero--;
+if (comprobarPrimo((int)$numero)) {
+	echo "<br>El número $numero es primo";
+}else{
+	echo "<br>El número $numero tampoco es primo";
 }
 
 
 
 // B) Genera una estructura para que haya al menos una clave cuyo valor represente uno de estos tipos de datos:
 
+define("NUMERO_LIBROS", 10);
+
+echo "<br>";
+
 function generarDato($tipo){
 	switch ($tipo) {
 		case 'cadena':
-			return 
+			$cadena = "";
+			for ($i=0; $i < rand(1, 10); $i++) { 
+				$char = chr(rand(97, 122));
+				$cadena .= $char;
+			}
+			return $cadena;
 			break;
-		
+		case 'entero':
+			return rand(100, 99999999);
+			break;
+		case 'decimal':
+			$num= rand(1, 999) + (rand(0, 99999) / 100000);
+			return $num;
+			break;
+		case 'fecha':
+			return rand(1, 28)."/".rand(1, 9)."/"."2025";
+			break;
 		default:
 			// code...
 			break;
 	}
 }
+$catalogo = array();
+for ($i=0; $i < NUMERO_LIBROS; $i++) { 
+	$libro = array(
+    "titulo" => generarDato("cadena"),
+    "n_paginas" => generarDato("entero"),
+    "precio" => generarDato("decimal"),
+    "fecha_publicacion" => generarDato("fecha"),
+);
+	$catalogo[] = $libro;
+}
 
-$catalogo = array(
-    "titulo" => "cadena",
-    "n_paginas" => "entero",
-    "precio" => "decimal",
-    "fecha_publicacion" => "fecha");
+foreach ($catalogo as $libro) {
+	echo "<br>";
+	foreach ($libro as $key => $value) {
+		echo "$key : $value <br>";
+	}
+}
