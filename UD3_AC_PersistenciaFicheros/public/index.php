785429ea7252d09f2f09ba52c637a218c016158e
MarcosRSPG <marcosrs.softwaredeveloper@gmail.com>
2025-12-02 17:51:57 +0100

    ejercicio files

---
+++ UD3_AC_PersistenciaFicheros/public/index.php
@@ -0,0 +1,61 @@
+<?php
+
+$alfabeto = range('a', 'z');
+$nombreFichero = 'alfabeto.txt';
+
+// 1. Crear el fichero alfabeto.txt
+$f = fopen($nombreFichero, 'w');
+
+$contador = 0;
+foreach ($alfabeto as $letra) {
+    ++$contador;
+
+    fwrite($f, $letra);
+
+    if ($contador % 5 == 0) {
+        fwrite($f, PHP_EOL);
+    } else {
+        fwrite($f, ' ');
+    }
+}
+
+fclose($f);
+
+$f = fopen($nombreFichero, 'r');
+
+if (!is_dir('letras')) {
+    mkdir('letras');
+}
+if (!is_dir('copiasletras')) {
+    mkdir('copiasletras');
+}
+
+while (($datos = fscanf($f, '%s%s%s%s%s')) !== false) {
+    foreach ($datos as $letra) {
+        if (is_null($letra)) {
+            continue;
+        }
+        $archivoLetra = "letras/$letra.txt";
+        $fl = fopen($archivoLetra, 'w');
+        fwrite($fl, $letra);
+        fclose($fl);
+    }
+}
+
+rewind($f);
+
+while (($datos = fscanf($f, '%s%s%s%s%s')) !== false) {
+    foreach ($datos as $letra) {
+        if (is_null($letra)) {
+            continue;
+        }
+        $origen = "letras/$letra.txt";
+        $destino = "copiasletras/$letra.txt";
+
+        if (file_exists($origen)) {
+            copy($origen, $destino);
+        }
+    }
+}
+
+fclose($f);
---
+++ UD3_AC_PersistenciaFicheros/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mrs/ud3_ac_persistencia-ficheros',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '200029dda64149a59b4a34dc1bf4abee88d471f2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mrs/ud3_ac_persistencia-ficheros' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '200029dda64149a59b4a34dc1bf4abee88d471f2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
